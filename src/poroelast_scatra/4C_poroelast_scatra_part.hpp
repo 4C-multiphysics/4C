12e1f1a86352a6946c3e517934cea51eb0c8aed7
vryy <giang.bui@hereon.de>
2024-11-22 10:51:35 +0100

    Adapt to std::shared_ptr

--- src/poroelast_scatra/4C_poroelast_scatra_part.hpp
+++ src/poroelast_scatra/4C_poroelast_scatra_part.hpp
@@ -64,7 +64,7 @@ namespace PoroElastScaTra
 
    protected:
     //! store contact nitsche strategy for ssi problems
-    Teuchos::RCP<CONTACT::NitscheStrategySsi> contact_strategy_nitsche_;
+    std::shared_ptr<CONTACT::NitscheStrategySsi> contact_strategy_nitsche_;
   };
 }  // namespace PoroElastScaTra
 
