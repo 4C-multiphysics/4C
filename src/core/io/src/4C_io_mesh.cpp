d4b6363a4b446cdf613479c03a59cd1d4fd2b09e
Amadeus Gebauer <amadeus.gebauer@tum.de>
2025-09-04 18:04:45 +0200

    Let IMR read ids of cells and points from file

--- src/core/io/src/4C_io_mesh.cpp
+++ src/core/io/src/4C_io_mesh.cpp
@@ -10,6 +10,10 @@
 #include "4C_utils_enum.hpp"
 #include "4C_utils_std23_unreachable.hpp"
 
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+
 
 
 FOUR_C_NAMESPACE_OPEN
@@ -32,63 +36,45 @@ std::string Core::IO::MeshInput::describe(VerbosityLevel level)
   std23::unreachable();
 }
 
-void Core::IO::MeshInput::print(const Mesh& mesh, std::ostream& os, VerbosityLevel verbose)
+template <unsigned dim>
+void Core::IO::MeshInput::print(const Mesh<dim>& mesh, std::ostream& os, VerbosityLevel verbose)
 {
   if (verbose >= VerbosityLevel::summary)
   {
     auto num_elements = std::accumulate(mesh.cell_blocks.begin(), mesh.cell_blocks.end(), 0,
-        [](int sum, const auto& block) { return sum + block.second.cell_connectivities.size(); });
+        [](int sum, const auto& block) { return sum + block.second.size(); });
 
-    os << "  consists of " << mesh.points.size() << " points and " << num_elements << " cells; \n"
-       << "  organized in " << mesh.cell_blocks.size() << " cell-blocks, ";
-    os << mesh.point_sets.size() << " point-sets and ";
-    os << mesh.side_sets.size() << " side-sets. ";
-    os << std::endl << std::endl;
+    os << "Mesh consists of " << mesh.points.size() << " points and " << num_elements
+       << " cells organized in " << mesh.cell_blocks.size() << " cell-blocks and "
+       << mesh.point_sets.size() << " point-sets.\n\n";
   }
   if (verbose >= VerbosityLevel::detailed_summary)
   {
     if (verbose == VerbosityLevel::full)
     {
-      for (const auto& [point_id, coords] : mesh.points)
+      for (const auto& point : mesh.points)
       {
-        os << "Point " << point_id << ": ";
-        for (const auto& coord : coords)
+        os << "  " << point.external_id << ": [";
+        for (const auto& coord : point.coords)
         {
           os << std::format("{:10.6g},", coord);
         }
-        os << "\n";
+        os << "]\n";
       }
-      os << std::endl;
+      os << "\n";
     }
     os << "cell-blocks:\n";
-    os << "------------\n";
     for (const auto& [id, connectivity] : mesh.cell_blocks)
     {
-      os << "cell-block " << id << ": ";
+      os << "  cell-block " << id << ": ";
       print(connectivity, os, verbose);
     }
+    os << "\n";
 
-    os << "\nside-sets:\n";
-    os << "-----------\n";
-    for (const auto& [ss_id, ss] : mesh.side_sets)
-    {
-      os << "side-set " << ss_id << ": ";
-      print(ss, os, verbose);
-    }
-
-    os << "\nline-sets:\n";
-    os << "----------\n";
-    for (const auto& [ls_id, ls] : mesh.line_sets)
-    {
-      os << "line-set " << ls_id << ": ";
-      print(ls, os, verbose);
-    }
-
-    os << "\npoint-sets:\n";
-    os << "-------------\n";
+    os << "point-sets:\n";
     for (const auto& [ps_id, ps] : mesh.point_sets)
     {
-      os << "point-set " << ps_id << ": ";
+      os << "  point-set " << ps_id << ": ";
       print(ps, os, verbose);
     }
   }
@@ -98,91 +84,39 @@ void Core::IO::MeshInput::print(const CellBlock& block, std::ostream& os, Verbos
 {
   using EnumTools::operator<<;
 
-  os << block.cell_connectivities.size() << " Cells of shape " << block.cell_type;
-  os << std::endl;
+  os << block.size() << " cells of type " << block.cell_type << "\n";
 
-  if (verbose == VerbosityLevel::detailed)
-  {
-    int nline = 0;
-    os << "cells:";
-    for (const auto& [id, connectivity] : block.cell_connectivities)
-    {
-      if (nline++ % 12 == 0) os << "\n  ";
-      os << id << ",";
-    }
-    if (nline % 12 != 0) os << std::endl;
-    os << std::endl;
-  }
-  else if (verbose == VerbosityLevel::full)
-  {
-    for (const auto& [id, conn] : block.cell_connectivities)
-    {
-      os << "  cell " << id << ": ";
-      for (const auto& point_id : conn)
-      {
-        os << point_id << ",";
-      }
-      os << std::endl;
-    }
-    os << std::endl;
-  }
-}
-
-void Core::IO::MeshInput::print(const SideSet& side_set, std::ostream& os, VerbosityLevel verbose)
-{
-  os << " contains " << side_set.sides.size() << " sides";
   if (verbose == VerbosityLevel::full)
   {
-    os << ": ";
-    int nline = 0;
-    for (const auto& [id, _] : side_set.sides)
+    for (const auto& [external_id, connectivity] : block.cells())
     {
-      if (nline++ % 12 == 0) os << "\n  ";
-      os << std::setw(5) << id << ",";
+      os << "    " << external_id << ": [";
+      for (const auto& id : connectivity) os << id << ", ";
+      os << "]\n";
     }
-    if (nline % 12 != 0) os << std::endl;
-  }
-  else
     os << "\n";
-}
-
-void Core::IO::MeshInput::print(const LineSet& line_set, std::ostream& os, VerbosityLevel verbose)
-{
-  os << " contains " << line_set.lines.size() << " lines";
-  if (verbose == VerbosityLevel::full)
-  {
-    os << ": ";
-    int nline = 0;
-    for (const auto& [id, _] : line_set.lines)
-    {
-      if (nline++ % 12 == 0) os << "\n  ";
-      os << std::setw(5) << id << ",";
-    }
-    if (nline % 12 != 0) os << std::endl;
   }
-  else
-    os << "\n";
 }
 
 void Core::IO::MeshInput::print(const PointSet& point_set, std::ostream& os, VerbosityLevel verbose)
 {
-  os << " contains " << point_set.point_ids.size() << " points";
-  if (verbose == VerbosityLevel::full)
+  os << point_set.point_ids.size() << " points";
+  if (verbose == VerbosityLevel::full && point_set.point_ids.size() > 0)
   {
-    os << ": ";
     int nline = 0;
-    int nodelength =
-        std::to_string(*std::max_element(point_set.point_ids.begin(), point_set.point_ids.end()))
-            .size();
+    int nodelength = std::to_string(*std::ranges::max_element(point_set.point_ids)).size();
     for (const int id : point_set.point_ids)
     {
       if (nline++ % 12 == 0) os << "\n  ";
-      os << std::setw(nodelength) << id << ",";
+      os << " " << std::setw(nodelength) << id << ",";
     }
-    if (nline % 12 != 0) os << std::endl;
+    if (nline % 12 != 0) os << "\n";
   }
-  else
-    os << "\n";
+
+  os << "\n";
 }
+
+template void Core::IO::MeshInput::print(
+    const Mesh<3>& mesh, std::ostream& os, VerbosityLevel verbose);
 
 FOUR_C_NAMESPACE_CLOSE
