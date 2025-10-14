44198219fb564029df3ef114c1e8d27f92388d43
Max Firmbach <max.firmbach@unibw.de>
2025-10-14 15:23:08 +0200

    Fix formatting

--- tests/list_of_tests.cmake
+++ tests/list_of_tests.cmake
@@ -1,4 +1,4 @@
-    # This file is part of 4C multiphysics licensed under the
+# This file is part of 4C multiphysics licensed under the
 # GNU Lesser General Public License v3.0 or later.
 #
 # See the LICENSE.md file in the top-level for license information.
