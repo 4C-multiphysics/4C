555013c792e76f82aeff42bb80fd255965f04fe1
Martin Frank <martin.frank@unibw.de>
2025-10-17 17:02:09 +0200

    Fix porofluid_x_scatra maps

--- src/porofluid_pressure_based_elast_scatra/4C_porofluid_pressure_based_elast_scatra_partitioned.cpp
+++ src/porofluid_pressure_based_elast_scatra/4C_porofluid_pressure_based_elast_scatra_partitioned.cpp
@@ -146,10 +146,39 @@ void PoroPressureBased::PorofluidElastScatraPartitionedAlgorithm::iter_update_st
   porofluid_inc_np_->update(1.0, *porofluid_elast_algo()->fluid_phinp(), 0.0);
   if (artery_coupling_active_)
   {
-    artery_pressure_inc_np_->update(
-        1.0, *(porofluid_elast_algo()->porofluid_algo()->art_net_tim_int()->pressurenp()), 0.0);
-    artery_scatra_inc_np_->update(
-        1.0, *(scatra_meshtying_strategy_->art_scatra_field()->phinp()), 0.0);
+    auto target_pressure_map = artery_pressure_inc_np_->get_map();
+    auto source_pressure_map =
+        porofluid_elast_algo()->porofluid_algo()->art_net_tim_int()->pressurenp()->get_map();
+    if (target_pressure_map.same_as(source_pressure_map))
+    {
+      artery_pressure_inc_np_->update(
+          1.0, *(porofluid_elast_algo()->porofluid_algo()->art_net_tim_int()->pressurenp()), 0.0);
+    }
+    else
+    {
+      Core::LinAlg::Vector<double> tmp_pressure_vector(target_pressure_map, true);
+      Core::LinAlg::Import pressure_import(target_pressure_map, source_pressure_map);
+      tmp_pressure_vector.import(
+          *porofluid_elast_algo()->porofluid_algo()->art_net_tim_int()->pressurenp(),
+          pressure_import, Insert);
+      artery_pressure_inc_np_->update(1.0, tmp_pressure_vector, 0.0);
+    }
+
+    auto target_map = artery_scatra_inc_np_->get_map();
+    auto src_vec = scatra_meshtying_strategy_->art_scatra_field()->phinp();
+    auto source_map = src_vec->get_map();
+
+    if (target_map.same_as(source_map))
+    {
+      artery_scatra_inc_np_->update(1.0, *src_vec, 0.0);
+    }
+    else
+    {
+      Core::LinAlg::Vector<double> tmp(target_map, /*zeroOut=*/true);
+      Core::LinAlg::Import imp(target_map, source_map);
+      tmp.import(*src_vec, imp, Insert);
+      artery_scatra_inc_np_->update(1.0, tmp, 0.0);
+    }
   }
 
 }  // iter_update_states()
@@ -186,10 +215,39 @@ bool PoroPressureBased::PorofluidElastScatraPartitionedAlgorithm::convergence_ch
   porofluid_inc_np_->update(1.0, *(porofluid_elast_algo()->fluid_phinp()), -1.0);
   if (artery_coupling_active_)
   {
-    artery_pressure_inc_np_->update(
-        1.0, *(porofluid_elast_algo()->porofluid_algo()->art_net_tim_int()->pressurenp()), -1.0);
-    artery_scatra_inc_np_->update(
-        1.0, *(scatra_meshtying_strategy_->art_scatra_field()->phinp()), -1.0);
+    auto target_pressure_map = artery_pressure_inc_np_->get_map();
+    auto source_pressure_map =
+        porofluid_elast_algo()->porofluid_algo()->art_net_tim_int()->pressurenp()->get_map();
+    if (target_pressure_map.same_as(source_pressure_map))
+    {
+      artery_pressure_inc_np_->update(
+          1.0, *(porofluid_elast_algo()->porofluid_algo()->art_net_tim_int()->pressurenp()), 0.0);
+    }
+    else
+    {
+      Core::LinAlg::Vector<double> tmp_pressure_vector(target_pressure_map, true);
+      Core::LinAlg::Import pressure_import(target_pressure_map, source_pressure_map);
+      tmp_pressure_vector.import(
+          *porofluid_elast_algo()->porofluid_algo()->art_net_tim_int()->pressurenp(),
+          pressure_import, Insert);
+      artery_pressure_inc_np_->update(1.0, tmp_pressure_vector, -1.0);
+    }
+
+    auto target_map = artery_scatra_inc_np_->get_map();
+    auto src_vec = scatra_meshtying_strategy_->art_scatra_field()->phinp();
+    auto source_map = src_vec->get_map();
+
+    if (target_map.same_as(source_map))
+    {
+      artery_scatra_inc_np_->update(1.0, *src_vec, 0.0);
+    }
+    else
+    {
+      Core::LinAlg::Vector<double> tmp(target_map, /*zeroOut=*/true);
+      Core::LinAlg::Import imp(target_map, source_map);
+      tmp.import(*src_vec, imp, Insert);
+      artery_scatra_inc_np_->update(1.0, tmp, -1.0);
+    }
   }
 
   // build the L2-norm of the scalar increment and the scalar
