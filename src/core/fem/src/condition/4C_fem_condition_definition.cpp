7f4f3675ca9a138423736c52c6757ed2d6ed2400
Rasmus Joussen <rasmus.joussen@tum.de>
2026-01-28 12:17:30 +0100

    Read and define BCs by node_set_name

--- src/core/fem/src/condition/4C_fem_condition_definition.cpp
+++ src/core/fem/src/condition/4C_fem_condition_definition.cpp
@@ -15,6 +15,7 @@
 
 #include <algorithm>
 #include <iterator>
+#include <optional>
 #include <utility>
 
 FOUR_C_NAMESPACE_OPEN
@@ -38,12 +39,17 @@ Core::Conditions::ConditionDefinition::ConditionDefinition(std::string sectionna
   using namespace Core::IO::InputSpecBuilders;
   // Add common parameters to all conditions.
 
-  add_component(
-      parameter<int>("E", {.description = "ID of the condition. This ID refers to the respective "
-                                          "topological entity of the condition."}));
-  add_component(parameter<Core::Conditions::EntityType>(
-      "ENTITY_TYPE", {.description = "The type of entity that E refers to.",
-                         .default_value = Core::Conditions::EntityType::legacy_id}));
+  add_component(parameter<std::optional<int>>(
+      "E", {.description = "ID of the condition. This ID refers to the respective "
+                           "topological entity of the condition. Not allowed if ENTITY_TYPE is "
+                           "NODE_SET_NAME."}));
+  add_component(parameter<std::optional<std::string>>("NODE_SET_NAME",
+      {.description = "This refers to the respective node set name in the external mesh file. Only "
+                      "allowed if ENTITY_TYPE is NODE_SET_NAME and no E: ID is given."}));
+  add_component(parameter<Core::Conditions::EntityType>("ENTITY_TYPE",
+      {.description = "The type of entity identifier being used. Refers to E for ID-based "
+                      "identification or to NODE_SET_NAME for name-based identification.",
+          .default_value = Core::Conditions::EntityType::legacy_id}));
 }
 
 
@@ -64,7 +70,8 @@ void Core::Conditions::ConditionDefinition::add_component(const Core::IO::InputS
 /*----------------------------------------------------------------------*
  *----------------------------------------------------------------------*/
 void Core::Conditions::ConditionDefinition::read(Core::IO::InputFile& input,
-    std::multimap<int, std::shared_ptr<Core::Conditions::Condition>>& cmap) const
+    std::multimap<int, std::shared_ptr<Core::Conditions::Condition>>& cmap,
+    const std::map<std::string, std::vector<int>>& node_sets_names) const
 {
   Core::IO::InputParameterContainer container;
   try
@@ -81,20 +88,67 @@ void Core::Conditions::ConditionDefinition::read(Core::IO::InputFile& input,
   for (const auto& condition_data :
       container.get_or<std::vector<Core::IO::InputParameterContainer>>(section_name(), {}))
   {
+    // get entity_type, id, node_set_name from input
     auto entity_type = condition_data.get<EntityType>("ENTITY_TYPE");
+    auto id = condition_data.get<std::optional<int>>("E");
+    auto node_set_name = condition_data.get<std::optional<std::string>>("NODE_SET_NAME");
 
-    int id = condition_data.get<int>("E");
-    // Legacy IDs are read as 1-based, but internally we use 0-based IDs.
-    if (entity_type == EntityType::legacy_id) id -= 1;
+    int resolved_id = resolve_entity_id(entity_type, id, node_set_name, node_sets_names);
 
-    std::shared_ptr<Core::Conditions::Condition> condition =
-        std::make_shared<Core::Conditions::Condition>(
-            id, condtype_, buildgeometry_, gtype_, entity_type);
+    auto condition = std::make_shared<Core::Conditions::Condition>(
+        resolved_id, condtype_, buildgeometry_, gtype_, entity_type, node_set_name);
+
     condition->parameters() = condition_data;
 
-    cmap.emplace(id, condition);
+    cmap.emplace(resolved_id, condition);
   }
 }
+
+int Core::Conditions::ConditionDefinition::resolve_entity_id(EntityType entity_type,
+    std::optional<int> id, const std::optional<std::string>& node_set_name,
+    const std::map<std::string, std::vector<int>>& node_sets_names) const
+{
+  if (entity_type == EntityType::node_set_name)
+  {
+    FOUR_C_ASSERT_ALWAYS(node_set_name.has_value(),
+        "{} condition of entity type NODE_SET_NAME requires a non-empty NODE_SET_NAME.", condtype_);
+
+    FOUR_C_ASSERT_ALWAYS(!id.has_value(),
+        "{} condition of entity type NODE_SET_NAME must not specify an ID via E: ID.", condtype_);
+
+    FOUR_C_ASSERT_ALWAYS(node_sets_names.contains(node_set_name.value()),
+        "Cannot apply {} condition with external name '{}' which is not specified in the mesh "
+        "file.",
+        condtype_, node_set_name.value());
+
+    const auto& ids = node_sets_names.at(node_set_name.value());
+    FOUR_C_ASSERT_ALWAYS(ids.size() == 1,
+        "Cannot apply {} condition with external name '{}' which is not unique in the mesh file "
+        "({} occurrences found).",
+        condtype_, node_set_name.value(), ids.size());
+    return ids[0];
+  }
+
+  // Other entity types
+  FOUR_C_ASSERT_ALWAYS(id.has_value() && id.value() >= 0,
+      "{} condition of entity type {} requires a non-negative ID specified via E: ID.", condtype_,
+      entity_type);
+
+  FOUR_C_ASSERT_ALWAYS(!node_set_name.has_value(),
+      "{} condition of entity type {} must not specify a NODE_SET_NAME.", condtype_, entity_type);
+
+  // Legacy IDs are read as 1-based, but internally we use 0-based IDs.
+  if (entity_type == EntityType::legacy_id)
+  {
+    FOUR_C_ASSERT_ALWAYS(id.value() > 0,
+        "{} condition of entity type legacy_id requires a positive ID specified via E: ID.",
+        condtype_);
+    return id.value() - 1;
+  }
+
+  return id.value();
+}
+
 
 
 Core::IO::InputSpec Core::Conditions::ConditionDefinition::spec() const
