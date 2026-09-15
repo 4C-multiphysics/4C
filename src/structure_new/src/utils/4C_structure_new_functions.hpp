db90539acd1cae4e486a6cb175efc714cb2a3169
Mélissandre <melissandre.staub-milants@tum.de>
2026-09-15 16:44:45 +0200

    Enable to define a spatially varying YOUNG's field for MAT_Struct_StVenantKirchhoff materials

--- src/structure_new/src/utils/4C_structure_new_functions.hpp
+++ src/structure_new/src/utils/4C_structure_new_functions.hpp
@@ -54,10 +54,16 @@ namespace Solid
     std::vector<double> evaluate_time_derivative(
         std::span<const double> x, double t, unsigned deg, std::size_t component) const override;
 
+    // Case with spatially varying material properties (inputfield)
+    double evaluate(std::span<const double> x, double t, std::size_t component, int eleGID) const override;
+    
+    std::vector<double> evaluate_time_derivative(
+        std::span<const double> x, double t, unsigned deg, std::size_t component, int eleGID) const override;
+
     [[nodiscard]] std::size_t number_components() const override { return (2); };
 
    private:
-    double youngmodulus_;
+    Core::IO::InputField<double> youngmodulus_;
     double poissonratio_;
     double strucdensity_;
   };
