5e5fef28fc444fb36d27c2412aca8d415352eefb
Max Firmbach <a11bmafi@sisyphos.bauv.unibw-muenchen.de>
2026-09-18 15:08:35 +0000

    Add LM map to reorder maps for Teko

--- src/beaminteraction/src/4C_beaminteraction_str_model_evaluator.cpp
+++ src/beaminteraction/src/4C_beaminteraction_str_model_evaluator.cpp
@@ -883,9 +883,29 @@ bool Solid::ModelEvaluator::BeamInteractionModelEvaluator::assemble_jacobian(
     auto kappa_inv = beam_contact_model->get_penalty_kappa_inverse();
     auto structure_solver = tim_int().get_data_sdyn().get_lin_solvers().at(Solid::model_structure);
 
-    structure_solver->params()
-        .sublist("Teko Parameters")
-        .set<std::shared_ptr<Core::LinAlg::Vector<double>>>("scaling vector", kappa_inv);
+    auto& teko_parameters = structure_solver->params().sublist("Teko Parameters");
+    teko_parameters.set<std::shared_ptr<Core::LinAlg::Vector<double>>>("scaling vector", kappa_inv);
+
+    auto reorder_maps =
+        teko_parameters.get<std::vector<std::shared_ptr<const Core::LinAlg::Map>>>("reorder: maps");
+    auto lambda_map = beam_contact_model->get_lagrange_map();
+
+    if (reorder_maps.size() == 2)
+    {
+      reorder_maps.emplace_back(lambda_map);
+    }
+    else if (reorder_maps.size() == 3)
+    {
+      reorder_maps[2] = lambda_map;
+    }
+    else
+    {
+      FOUR_C_THROW("Expected two or three beam-interaction reorder maps, but received {}.",
+          reorder_maps.size());
+    }
+
+    teko_parameters.set<std::vector<std::shared_ptr<const Core::LinAlg::Map>>>(
+        "reorder: maps", reorder_maps);
   }
 
   // no need to keep it
